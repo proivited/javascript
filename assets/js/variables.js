49e362094e43236ae5dff645ccf5666b7ccb7888
proivited <miguel.alexis.gomez.a@gmail.com>
2021-06-15 16:59:09 -0500

    variable

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -1,4 +1,4 @@
-var variable1 = 'soy la variable 1';
+/* var variable1 = 'soy la variable 1';
 console.log(variable1);
 variable1 = 1212;
 console.log(variable1);
@@ -7,6 +7,26 @@ const variable2 = 'variable2'; // a las constantes no les puedes cambiar el valo
 console.log(variable2);
 variable2 = 10;
 console.log(variable2);
+
+const nuevaFuncion = () => {
+    var x = 31;
+    if(true){
+        console.log(x);
+
+    }
+    console.log(x);
+}
+
+nuevaFuncion();*/
+
+const nombre ='Carlos';
+const apellidos = 'Jesus Leon';
+
+console.log('Hola, mi nombre es:' + nombre + 'mis apellidos son' + apellidos);
+console.log(`Hola mi nombre es ${nombre} y mis apellidos son ${apellidos}`); //templatestream
+
+const arreglo = [1,2,3,4,5];
+con
 
 
 
