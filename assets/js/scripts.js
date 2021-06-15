c11661b7449186e6cca330576149ffc6870a725d
proivited <miguel.alexis.gomez.a@gmail.com>
2021-06-15 15:20:47 -0500

    variables

---
+++ assets/js/scripts.js
@@ -0,0 +1,26 @@
+var dato1 = 24;
+var dato2 = "Holaaaa";
+var dato3 = "false";
+var dato4 = "5.37";
+var dato5 = true;
+var dato6 = null;
+var dato7 = "Hoy es un lindo dia";
+var dato8 = 829;
+var dato9 = "null";
+var dato10 = false;
+
+var suma = parseInt(dato4)  + dato8;
+console.log('suma', suma)
+
+console.log('dato1', typeof(dato1));
+console.log('dato2', typeof(dato2));
+console.log('dato3', typeof(dato3));
+console.log('dato4', typeof(parseFloat(dato4)));
+console.log('dato4', typeof(parseInt(dato4)),parseInt(dato4));
+console.log('dato5', typeof(dato5));
+console.log('dato6', typeof(dato6));
+console.log('dato7', typeof(dato7));
+console.log('dato8', typeof(dato8));
+console.log('dato9', typeof(dato9));
+console.log('dato10', typeof(dato10));
+console.log('dato11', typeof(dato11));
---
+++ assets/js/variables.js
@@ -0,0 +1,12 @@
+var variable1 = 'soy la variable 1';
+console.log(variable1);
+variable1 = 1212;
+console.log(variable1);
+
+const variable2 = 'variable2'; // a las constantes no les puedes cambiar el valor pero ahorran espacio
+console.log(variable2);
+variable2 = 10;
+console.log(variable2);
+
+
+
